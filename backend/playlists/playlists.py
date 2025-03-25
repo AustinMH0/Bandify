913a4df86071bd0e529e26ebaa9f40c1611ed684
AustinMH0 <austinmhendricks@gmail.com>
2025-03-25 16:23:05 -0700

    playlists.py now returns a list of the user's playlists and all the tracks contained within each playlist

--- backend/playlists/playlists.py
+++ backend/playlists/playlists.py
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = os.urandom(64)
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 redirect_uri = 'http://localhost:5000/callback'
-scope = 'playlist-read-private'
+scope = 'playlist-read-private playlist-read-collaborative'
 
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
@@ -28,7 +28,8 @@ sp_oauth = SpotifyOAuth(
     show_dialog=True
 )
 
-sp = Spotify(auth_manager=sp_oauth)
+sp = Spotify(auth_manager=sp_oauth, requests_timeout=10)
+
 
 @app.route('/')
 def home():
@@ -42,17 +43,36 @@ def callback():
     sp_oauth.get_access_token(request.args['code'])
     return redirect(url_for('get_playlists'))
 
+def show_tracks(results):
+    track_list = []
+    for i, item in enumerate(results['items']):
+        track = item['track']
+        track_list.append(f"{i+1}. {track['artists'][0]['name']} - {track['name']}")
+    return '<br>'.join(track_list)
+
 @app.route('/get_playlists')
 def get_playlists():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
-    
-    playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
 
-    return playlists_html
+    playlists = sp.current_user_playlists()
+    user_id = sp.me()['id']
+    res = ''
+
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == user_id:
+            res += f"<h3>{playlist['name']}</h3>"
+            res += f"<p>Total tracks: {playlist['tracks']['total']}</p>"
+
+            tracks = sp.playlist_items(playlist['id'], fields="items,next", additional_types=('track', ))
+            res += show_tracks(tracks)  
+
+            while tracks['next']:
+                tracks = sp.next(tracks)
+                res += show_tracks(tracks)  
+
+    return res
 
 @app.route('/logout')
 def logout():
