3e5ea41df7b2876aca8fa9c19971fc72b91e1fc5
Chito7 <luisrt720@gmail.com>
2025-04-08 17:46:50 -0700

    Refactored bandcamp_search.py
    Replaced selenium with requests to grab html content and parse/scrape using beautiful soup as done so previously by Austin H.

--- backend/search/bandcamp_search.py
+++ backend/search/bandcamp_search.py
@@ -1,13 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
+
 class BandcampSearch:
 
     def __init__(self, track, artist):
         self.track = track
         self.artist = artist
-        self.driver = self.setup_driver()
+        #self.driver = self.setup_driver()
+
+        self.driver = None
+
 
     def setup_driver(self):
         options = Options()
@@ -30,10 +35,12 @@ class BandcampSearch:
 
     def get_track_url(self):
         search_url = f'https://bandcamp.com/search?q={self.track}+{self.artist}&item_type=t'
-        self.driver.get(search_url)
-        sleep(3) 
+        returned = requests.get(search_url)
+        
+        
+        
 
-        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        soup = BeautifulSoup(returned.content, 'html.parser')
         
         # Find first track result
         first_track = None
@@ -44,7 +51,7 @@ class BandcampSearch:
                 break
 
         if not first_track:
-            self.driver.quit()
+            #self.driver.quit()
             return None  
 
         heading = first_track.select_one(".heading a")
@@ -54,29 +61,79 @@ class BandcampSearch:
     
     def get_track_price(self, url):
         # Visit track page
-        self.driver.get(url)
-        sleep(2)
-        track_soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        # self.driver.get(url)
+        #sleep(2)
+        returned = requests.get(url)
+        track_soup = BeautifulSoup(returned.content, 'html.parser')
 
         price_span = track_soup.select_one('span.base-text-color')
         price = price_span.get_text(strip=True) if price_span else "Price not found"
 
-        self.driver.quit()
+        #self.driver.quit()
         return price
 
 
 # Example Usage:
 
-'''
-bc = BandcampSearch("asthma", "dierot")
+# import time
+# import queue
+# import threading
 
-url = bc.get_track_url()
-price = bc.get_track_price(url)
 
-print("Track price: " + price)
-print('\n')
-print("Track URL: " + url)
+# songsQueue = queue.Queue()
+    
+# songsQueue.put(("king von", "armed and dangerous")),
+# songsQueue.put(("king von", "took her to the o")),
 
-'''
 
+# songsQueue.put(("king von", "gleesh place")),
+# songsQueue.put(("king von", "crazy story")),
+
+
+# songsQueue.put(("king von", "twin nem")),
+# songsQueue.put(("lil durk", "green light")),
+
+
+# songsQueue.put(("lil baby", "we paid")),
+# songsQueue.put(("juice wrld", "syphilis")),
+    
+
+
+
+# Function to search for songs in a queue
+# def search_songs(songsQueue, results):
+#     while not songsQueue.empty():
+#         try:
+#             artist, song = songsQueue.get(timeout= 2)
+#         except queue.Empty:
+#             print("queue empty")
+#             return
+#         bc = BandcampSearch(song, artist)
+#         price = None
+#         url = bc.get_track_url()
+#         if url:
+#             price = bc.get_track_price(url)
+#         results.put((f"{artist} {song}", price))
+#         songsQueue.task_done()
+
+
+
+
+# Search for songs using multithreading
+# start = time.perf_counter()
+
+# results = queue.Queue()
+# for i in range(4):
+#     t = threading.Thread(target=search_songs, args=(songsQueue, results))
+#     t.start()
+
+# songsQueue.join()
+
+# end = time.perf_counter()
+
+# while not results.empty():
+#     song, price = results.get()
+#     print(f"{song} cost {price}")
+
+# print(end - start)
 
