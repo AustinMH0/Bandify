934b6f211c52be747d6331490f530ab197def178
AustinMH0 <austinmhendricks@gmail.com>
2025-04-16 18:23:18 -0700

    Cleaned up unused code/comments

--- backend/search/beatport_search.py
+++ backend/search/beatport_search.py
@@ -1,5 +1,4 @@
 from urllib.parse import quote
-import concurrent.futures
 from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
@@ -39,29 +38,3 @@ class BeatportSearch:
                     price = -1
 
         return {"price": price, "url": full_track_url}
-
-    # def search_multiple_tracks(self, tracks: list) -> list:
-    #     results = []
-    #     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    #         futures = {executor.submit(self.search_song, track['name'], track['artist']): track for track in tracks}
-    #         for future in concurrent.futures.as_completed(futures):
-    #             result = future.result()
-    #             results.append(result)
-    #     return results
-
-# # Example of how to use it
-# if __name__ == "__main__":
-#     bp = BeatportSearch()
-
-#     # Example list of tracks to search for
-#     tracks_to_search = [
-#         {"name": "Blood", "artist": "Boneless"},
-#         {"name": "Hypnotized", "artist": "AVOR"},
-#         {"name": "Clear Cut", "artist": "GLM"}
-#     ]
-
-#     # Get the results for all tracks
-#     results = bp.search_song("Blood", "Boneless")
-
-#     # Print the results
-#     print(results)
