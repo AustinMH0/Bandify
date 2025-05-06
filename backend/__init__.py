47091b9e53ae155d88bc481ed79a23e9cb0ee482
Chito7 <luisrt720@gmail.com>
2025-05-06 00:44:17 -0700

    server.py
    server.py now handles any queries to the database given an artist and track name. Queries in this version are still not parameterized.

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,17 @@
 from os import urandom
 from flask import Flask
 from flask_cors import CORS
+from sqlalchemy import create_engine
 
 from .playlists.playlists import playlists_bp
 from .playlists.itunes_search_routes import itunes_bp
 from .playlists.bandcamp_search_routes import bandCampBlueprint
 from .playlists.beatport_search_routes import beatport_bp
+from .playlists.server import server_bp
+
+# Instantiate SQLAlchemy engine 
+engine = create_engine('postgresql+psycopg2://postgres:password@127.0.0.1:5432/bandify')
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,5 +21,6 @@ def create_app():
     app.register_blueprint(itunes_bp)
     app.register_blueprint(bandCampBlueprint)
     app.register_blueprint(beatport_bp)
+    app.register_blueprint(server_bp)
 
     return app
